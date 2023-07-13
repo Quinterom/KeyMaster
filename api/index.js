5487c0b7c0203e689fbbf31a1c146ef0f44fc3bc
Quinterom <jlquinteromendez@gmail.com>
2023-07-13 11:54:58 +0200

    ...

--- api/index.js
+++ api/index.js
@@ -1,23 +1,53 @@
 // index.js (en tu función en Vercel)
 
-const { json, send } = require('micro');
-
 // Definir el enrutador de middleware
 const router = async (req, res) => {
-  if (req.method === 'POST' && req.url === '/api/guardar-datos') {
-    // Manejar la solicitud POST para guardar los datos en Airtable
-    const data = await json(req);
-    const palabrasPorMinuto = data.palabrasPorMinuto;
-
-    // Realizar las operaciones necesarias con las credenciales seguras
-    // ...
-
-    // Responder con una confirmación o cualquier otro resultado necesario
-    send(res, 200, { message: 'Datos guardados exitosamente' });
-  } else {
-    // Manejar otras rutas o métodos de solicitud si es necesario
-    send(res, 404, { message: 'No encontrado' });
-  }
-};
-
-module.exports = router;
+    if (req.method === 'POST' && req.url === '/api/guardar-datos') {
+      try {
+        // Manejar la solicitud POST para guardar los datos en Airtable
+        const data = await parseBody(req);
+        const palabrasPorMinuto = data.palabrasPorMinuto;
+  
+        // Realizar las operaciones necesarias con las credenciales seguras
+        // ...
+  
+        // Responder con una confirmación o cualquier otro resultado necesario
+        sendResponse(res, 200, { message: 'Datos guardados exitosamente' });
+      } catch (error) {
+        sendResponse(res, 500, { message: 'Error al procesar la solicitud' });
+      }
+    } else {
+      // Manejar otras rutas o métodos de solicitud si es necesario
+      sendResponse(res, 404, { message: 'No encontrado' });
+    }
+  };
+  
+  // Función para analizar el cuerpo de la solicitud
+  const parseBody = async (req) => {
+    return new Promise((resolve, reject) => {
+      let body = '';
+      req.on('data', (chunk) => {
+        body += chunk.toString();
+      });
+      req.on('end', () => {
+        try {
+          const data = JSON.parse(body);
+          resolve(data);
+        } catch (error) {
+          reject(error);
+        }
+      });
+      req.on('error', (error) => {
+        reject(error);
+      });
+    });
+  };
+  
+  // Función para enviar la respuesta
+  const sendResponse = (res, statusCode, body) => {
+    res.writeHead(statusCode, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(body));
+  };
+  
+  module.exports = router;
+  
